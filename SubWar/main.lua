a24d6586774cc01b2f5f06c238611dc5d5de17f0
orabillon <115501074+orabillon@users.noreply.github.com>
2023-09-14 18:54:52 +0200

    Création projet
    Création du projet

---
+++ SubWar/main.lua
@@ -0,0 +1,32 @@
+-- Débogueur Visual Studio Code tomblind.local-lua-debugger-vscode
+if pcall(require, "lldebugger") then
+    require("lldebugger").start()
+end
+
+-- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf("no")
+
+-- Empèche Love de filtrer les contours des images quand elles sont redimentionnées
+-- Indispensable pour du pixel art
+love.graphics.setDefaultFilter("nearest")
+
+love.window.setTitle("SubWar")
+
+function CheckCollision(x1, y1, w1, h1, x2, y2, w2, h2)
+    return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + h1
+end
+
+function love.load()
+    
+end
+
+function love.update(dt)
+   
+end
+
+function love.draw()
+   
+end
+
+function love.keypressed(key)
+end
